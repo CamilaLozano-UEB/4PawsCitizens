ea063f4a96fc085865f71a0f65422981a352cab4
NicolasP <NicolasP@192.168.0.9>
2021-02-18 18:15:39 -0500

    Creating the structure of the View class

--- src/co/edu/unbosque/FourPawsCitizens/view/View.java
+++ src/co/edu/unbosque/FourPawsCitizens/view/View.java
@@ -1,4 +1,20 @@
 package co.edu.unbosque.FourPawsCitizens.view;
 
+import java.util.Scanner;
+
 public class View {
+
+	private Scanner scan;
+
+	public View() {
+		scan = new Scanner(System.in);
+	}
+
+	public void printMessage(String message) {
+		System.out.println(message);
+	}
+
+	public String readInput() {
+		return scan.nextLine();
+	}
 }
