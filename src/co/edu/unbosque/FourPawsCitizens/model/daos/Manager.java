529eb92303ac193c092bf444da2954cf5184cd73
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2021-02-18 19:13:54 -0500

    Creation arraylist for class Pet, get, set and constructor

--- src/co/edu/unbosque/FourPawsCitizens/model/daos/Manager.java
+++ src/co/edu/unbosque/FourPawsCitizens/model/daos/Manager.java
@@ -1,5 +1,24 @@
 package co.edu.unbosque.FourPawsCitizens.model.daos;
 
+import java.util.ArrayList;
+
 public class Manager {
-	
+
+	private ArrayList<Pet> pet;
+
+	/**
+	 * @param pet
+	 */
+	public Manager(ArrayList<Pet> pet) {
+		this.setPet(pet);
+	}
+
+	public ArrayList<Pet> getPet() {
+		return pet;
+	}
+
+	public void setPet(ArrayList<Pet> pet) {
+		this.pet = pet;
+	}
+
 }
