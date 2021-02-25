18b4c7dd5c2f68b10d7b5f9b9df85c055bfa4e12
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2021-02-25 16:16:02 -0500

    Modifying IdentifierExistsException class

--- src/co/edu/unbosque/FourPawsCitizens/model/IdentifierExistsException.java
+++ src/co/edu/unbosque/FourPawsCitizens/model/IdentifierExistsException.java
@@ -1,10 +1,10 @@
 package co.edu.unbosque.FourPawsCitizens.model;
 
-public class ExistingIDException extends Exception {
+public class IdentifierExistsException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
-	public ExistingIDException() {
+	public IdentifierExistsException() {
 		super("La Id ya ha sido creada, joputa :3");
 	}
 }
