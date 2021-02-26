06cbc47912e6e2ad5ba5c09ef634599f8ffd6a69
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2021-02-26 13:29:07 -0500

    Modifyng text of IdentifierExistsException

--- src/co/edu/unbosque/FourPawsCitizens/Excepcions/IdentifierExistsException.java
+++ src/co/edu/unbosque/FourPawsCitizens/Excepcions/IdentifierExistsException.java
@@ -5,6 +5,6 @@ public class IdentifierExistsException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public IdentifierExistsException() {
-		super("La Id ya ha sido creada, joputa :3");
+		super("The ID has already been created");
 	}
 }
